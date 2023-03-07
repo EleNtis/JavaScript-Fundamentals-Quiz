a62d1e3a516cfe2abdb5a89d430f81746aa3dab7
Eleftheria Dispiraki <dispirakieleftheria@gmail.com>
2023-03-07 17:31:33 +0000

    added the questions

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+
+let questions= [
+   {
+    "text": "Which of the following type of variable is visible everywhere in your JavaScript code?",
+    "choices": ["Global variable", "Local variable", "Both of the above", "None of the above"],
+    "answer": "Global variable"
+   },
+   {
+    "text": "Which of the following function of String object combines the text of two strings and returns a new string?",
+    "choices": ["add ()", "merge()", "concat()", "append()"],
+    "answer": "add ()"
+   },
+   {
+    "text":"Which built-in method returns the length of the string?",
+    "choices": ["length()", "size()", "index()", "None of the above"],
+    "answer":"length()"
+   }, 
+   {
+    "text": "Which of the following function of Array object returns a string representing the array and its elements?",
+    "choices": ["toSource()", "toString()", "sort()", "splice()"],
+    "answer": "toString()"
+   },
+   {
+    "text": "Which built-in method returns the calling string value converted to upper case?",
+    "choices": ["toUpperCase()", "toUpper()", "changeCase(case)", "none of the above"],
+    "answer": "toUpperCase()"
+   }
+]
+
+
+
+
+
+
+
