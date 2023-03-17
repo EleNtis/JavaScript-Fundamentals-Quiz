6f87289343d8c64891f942e536648b68ec756f12
Eleftheria Dispiraki <dispirakieleftheria@gmail.com>
2023-03-17 15:03:32 +0000

    added functions for getting the questions and clicking on the choices

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,7 +7,7 @@ let startButton = document.querySelector("#start");
 let quizQuestions = document.querySelector("#questions");
 let timerInterval = document.querySelector("#time");
 let submitButton = document.querySelector("#submit");
-let choices = document.querySelector("#choices");
+let quizChoices = document.querySelector("#choices");
 let initials = document.querySelector("#initials");
 let feedback = document.querySelector("#feedback");
 
@@ -20,10 +20,42 @@ let wrongSoundEffect = new Audio ("assets/sfx/incorrect.wav");
 clock();
 
 
+
 //functions for the logic of the quiz
+function questionClick(){
+    if(this.value !== questions[questionIndex].answer){
+        timeLeft -= 15;
+    if(timeLeft < 0){
+        timeLeft = 0;
+    }
+
+    timerInterval.textContent = timeLeft;
+    
+    feedback.textContent = "Not quite"
+    wrongSoundEffect.play();
+    } else {
+    feedback.textContent = "Correct!"
+    rightSoundEffect.play();
+    }
+
+}
+
 function getQuestion(){
     let currentQuestion  = questions[questionIndex];
+    let questionTitle = document.querySelector("#question-title");
+    questionTitle.textContent = currentQuestion.text ; 
+    quizChoices.innerHTML = "";
+    currentQuestion.choices.forEach(function(choice, index) {
+        let choiceBtn = document.createElement("button");
+
+        choiceBtn.setAttribute("class", "choice");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = `${index + 1}. ${choice}`
+        choiceBtn.addEventListener("click", questionClick);
+        quizChoices.appendChild(choiceBtn);
+    })
 }
+
 
 
 function endQuiz(){
